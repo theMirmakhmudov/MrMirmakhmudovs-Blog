35b647be719076cfcd4d7c7d8b83b0c207f1eafb
mirmakhmudov16 <mr.mirmakhmudov16112008@gmail.com>
2024-06-07 12:14:35 +0500

    Commit

--- root/settings.py
+++ root/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-nw)_((0w!^f(#z&owe4d1cz8xuiby$%0o$_&gnheif)nr9)lv0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
