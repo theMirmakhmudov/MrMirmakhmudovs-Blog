0f3280cde65fb2e3d359da516b32af3fe6e29559
theMirmakhmudov <mr.mirmakhmudov16112008@gmail.com>
2025-01-18 21:19:18 +0500

    Big Upgrade

--- root/settings.py
+++ root/settings.py
@@ -1,5 +1,7 @@
 import os
 from pathlib import Path
+
+import dj_database_url
 from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -69,16 +71,8 @@ WSGI_APPLICATION = 'root.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv('DB_NAME'),
-        "USER": os.getenv('DB_USER'),
-        "PASSWORD": os.getenv('DB_PASS'),
-        "HOST": os.getenv('DB_HOST'),
-        "PORT": os.getenv('DB_PORT'),
-    }
+    'default': dj_database_url.config(default=f'postgres://postgres:mirmakhmudov16@localhost:5432/mirmakhmudovblog')
 }
 
 # Password validation
